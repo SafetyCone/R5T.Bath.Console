c5f4206a50f27e889b94e3ba2a222b469d065978
MinexAutomation <minex.automation@gmail.com>
2020-01-19 15:43:42 -0800

    Added ConsoleHumanOutput service implementation class type. Removed vestigial Class1 class type.

---
+++ source/R5T.Bath.Console/Code/Services/Implementations/ConsoleHumanOutput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using R5T.Bath.Default;
+
+
+namespace R5T.Bath.Console
+{
+    public class ConsoleHumanOutput : TextWriterHumanOutput
+    {
+        public ConsoleHumanOutput()
+            : base(System.Console.Out)
+        {
+        }
+    }
+}
