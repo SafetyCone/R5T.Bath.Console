74240623e5bcebeff32218ae621a71cf038e97cf
davidcoats <david@getsafetycone.com>
2022-04-27 15:25:32 -0700

    Interim changes.

--- source/R5T.Bath.Console/Code/Services/Implementations/ConsoleHumanOutput.cs
+++ source/R5T.Bath.Console/Code/Services/Implementations/ConsoleHumanOutput.cs
@@ -1,11 +1,14 @@
-﻿using System;
+using System;
 
 using R5T.Bath.Default;
+
+using R5T.T0064;
 
 
 namespace R5T.Bath.Console
 {
-    public class ConsoleHumanOutput : TextWriterHumanOutput
+    [ServiceImplementationMarker]
+    public class ConsoleHumanOutput : TextWriterHumanOutput, IHumanOutput, IServiceImplementation
     {
         public ConsoleHumanOutput()
             : base(System.Console.Out)
